b971414cd46657c22e471c0a927ae072954effa0
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-04-20 15:35:37 -0700

    pythonroom update

--- lightswitch.py
+++ lightswitch.py
@@ -1,1 +1,20 @@
 # author: akhileshcheguisthebestintheworld
+
+switch = False
+
+prisoners = range(1,101)
+rounds = range(1,101)
+
+for r in rounds:
+	for prisoner in prisoners:
+		#Check if prisoner is supposed to switch light
+		if prisoner % r == 0:
+			#Flip the switch
+			if switch == False:
+				switch = True
+			else:
+				switch = False
+if switch == True:
+	print "The switch is on!"
+if switch == False:
+	print "The switch is off!"
