0d4ab0203a4c28fc1b5473a34d0013753e9af681
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-04-27 15:24:10 -0700

    pythonroom update

--- circlesquare.py
+++ circlesquare.py
@@ -1,1 +1,17 @@
 # author: akhileshcheguisthebestintheworld
+import turtle
+
+turtle = turtle.Turtle()
+numbers = range(1,120)
+for number in numbers:
+	turtle.forward(3)
+	turtle.left(3)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(115)
+turtle.left(90)
+turtle.forward(115)
+turtle.left(90)
+turtle.forward(115)
+turtle.left(90)
+turtle.forward(55)
