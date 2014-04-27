eaad304369c4186c8a2862eba89213d05860bf27
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-04-27 14:34:22 -0700

    pythonroom update

--- numbers.py
+++ numbers.py
@@ -1,1 +1,7 @@
 # author: akhileshcheguisthebestintheworld
+numbers = range(1,501)
+total = 0
+for number in numbers:
+	if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
+		total = total + number
+print total
