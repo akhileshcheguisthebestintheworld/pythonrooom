790fe311c745b597340ccefd673ddd94ef41df4b
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-04-27 13:40:52 -0700

    pythonroom update

--- menu.py
+++ menu.py
@@ -1,1 +1,12 @@
 # author: akhileshcheguisthebestintheworld
+
+menu = ["pizza","pasta","cake"]
+
+for thing in menu:
+	answer = input("Do you like " + thing +"?")
+	if answer == "yes":
+		print "Have it" + "."
+	elif answer == "no":
+		print "Okay."
+	else:
+		print "I do not understand what that was" + "."
