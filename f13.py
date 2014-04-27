9e95352aeb16065c65f35ec6f38a116aa9dbc3c2
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-04-27 14:49:05 -0700

    pythonroom update

--- f13.py
+++ f13.py
@@ -6,10 +6,14 @@ month = 1
 year = 1900
 while year < 2015:
 	total = total + 1
-	if month == 12:
+	if month == 12 and date = 31:
 		year = year + 1
 		month = 1
+		date = 1
 	else:
-		month = month + 1
+		if date == 30:
+			month = month + 1
+		else:
+			date = date + 1
 		
 print total		
