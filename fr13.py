2ddfd67f9dded41d426419e65569e5fd82816688
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-04 13:42:12 -0700

    pythonroom update

--- fr13.py
+++ fr13.py
@@ -1,1 +1,15 @@
 # author: akhileshcheguisthebestintheworld
+years = range(1900,2015)
+count = 0
+months = range(1,13)
+
+for year in years:
+	for month in months:
+		if month = 1:
+			
+		days = range(1,32)
+		for day in days:
+			count = count + 1
+		
+print count	
+	
