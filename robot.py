6d71b212f636dfbf8f795e17be3c78557b908496
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-04 15:07:25 -0700

    initial commit

---
+++ robot.py
@@ -0,0 +1,24 @@
+# author: akhileshcheguisthebestintheworld
+
+a = 30
+b = 34
+
+
+answer = input("what operation do you want to do?")
+if answer == "add":
+	print a + b
+if answer == "subtract":
+	print a - b
+if answer == "multiply":
+	print a * b
+if answer == "divide":
+	print "Can't do it"
+
+answer = input("Guess what a and b are.")
+if answer == "30 and 34":
+	print "You are successful!!!!!"
+else:
+	print "You have failed the contest."
+			   
+
+
