2d5abdb9bb24304fda1f19b7016920b78ef05715
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-04 13:07:51 -0700

    initial commit

---
+++ joke.py
@@ -0,0 +1,21 @@
+# author: akhileshcheguisthebestintheworld
+total = 0
+
+date = 1
+month = 1
+year = 1900
+while year < 2015:
+	total = total + 1
+	
+	if month == 12 and date = 31:
+		year = year + 1
+		month = 1
+		date = 1
+	else:
+		if date == 30:
+			month = month + 1
+			date = 1
+		else:
+			date = date + 1
+		
+print total		
