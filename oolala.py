ce694aa754f2170fa36459b40b2991f366a2e83d
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-04 14:48:26 -0700

    initial commit

---
+++ oolala.py
@@ -0,0 +1,8 @@
+# author: akhileshcheguisthebestintheworld
+
+thing = "an irrational number"
+answer = input("What is pie equal to?")
+if answer != thing:
+	print "BWAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA!"
+else:
+	print "CCCCCOOOOOOOOORRRRRRRREEEEEEEECCCCCCTTTTTT!"
