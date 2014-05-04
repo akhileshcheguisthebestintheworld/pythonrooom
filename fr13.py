4d23e966f33d208d1099375832cbef16ce06e7c8
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-04 13:55:09 -0700

    pythonroom update

--- fr13.py
+++ fr13.py
@@ -5,9 +5,12 @@ months = range(1,13)
 
 for year in years:
 	for month in months:
-		if month = 1:
-			
-		days = range(1,32)
+		if month == 2:
+			days = range(1,29)
+		if month == 4 or month == 6 or month == 9 or month == 11:
+			days = range(1,31)
+		else:
+			days = range(1,32)
 		for day in days:
 			count = count + 1
 		
