89bae810cbb8ff1c355bc213989e2f45ef8f4be3
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-04 13:10:36 -0700

    pythonroom update

--- funny.py
+++ funny.py
@@ -1,21 +1,2 @@
 # author: akhileshcheguisthebestintheworld
-total = 0
-
-date = 1
-month = 1
-year = 1900
-while year < 2015:
-	total = total + 1
-	
-	if month == 12 and date = 31:
-		year = year + 1
-		month = 1
-		date = 1
-	else:
-		if date == 30:
-			month = month + 1
-			date = 1
-		else:
-			date = date + 1
-		
-print total		
+input("What is pie equal to?")	
