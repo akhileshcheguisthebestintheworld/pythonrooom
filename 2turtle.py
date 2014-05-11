7836fbeb8d66af42339d6fd66c99e0f056aa2005
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-11 13:38:05 -0700

    pythonroom update

--- 2turtle.py
+++ 2turtle.py
@@ -1,9 +1,4 @@
 import turtle
 
-t = turtle.Turtle()
-angle = 144
-numbers = range(1,100)
-for number in numbers:
-	t.backward(100)
-	t.left(angle)
-	angle = angle + 1
+t1 = turtle.Turtle()
+t2 = turtle.Turtle()
