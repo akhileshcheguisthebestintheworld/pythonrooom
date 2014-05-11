9322067d09c8d1b1fa2a65f1b3b53145ac732299
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-11 14:49:18 -0700

    initial commit

---
+++ newfile.py
@@ -0,0 +1,11 @@
+input("What is your favorite basketball team?")
+
+if answer == "the warriors":
+	input("Good, who is your favorite player?")
+	if answer == "stephen curry":
+		
+elif answer == "the spurs":
+	input("Okay, who is your favorite player?")
+elif answer == "the thunder":
+	input("Okay, who is your favorite player?")
+
