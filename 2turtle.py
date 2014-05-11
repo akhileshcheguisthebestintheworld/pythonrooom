0b3e13073a0b90cc6081e59529ede93f169ef8d1
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-11 14:25:36 -0700

    pythonroom update

--- 2turtle.py
+++ 2turtle.py
@@ -1,4 +1,11 @@
-import turtle
+input("What is your favorite basketball team?")
 
-t1 = turtle.Turtle()
-t2 = turtle.Turtle()
+if answer == "the warriors":
+	input("Good, who is your favorite player?")
+	if answer == "stephen curry":
+		
+elif answer == "the spurs":
+	input("Okay, who is your favorite player?")
+elif answer == "the thunder":
+	input("Okay, who is your favorite player?")
+
