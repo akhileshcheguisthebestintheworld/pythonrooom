9022aceea8ced75f9afc973179aad7e3fdbce328
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-16 20:25:31 -0700

    pythonroom update

--- crazy.py
+++ crazy.py
@@ -1,11 +1,31 @@
 input("What is your favorite basketball team?")
 
 if answer == "the warriors":
-	input("Good, who is your favorite player?")
+	input("Good, who is your favorite player on the Warriors?")
 	if answer == "stephen curry":
-		
+		input("Is he the best player currently in basketball?")
+		if answer == "yes":
+			input("I agree with you.")
+		else:
+			input("I do not agree with you.")
+	else:
+		input("I do not agree with you")
 elif answer == "the spurs":
-	input("Okay, who is your favorite player?")
+	input("Okay, who is your favorite player on the Spurs?")
+	if answer == "danny green":
+		input("I agree with you.")
+	elif answer == "tony parker":
+		input("I do not agree with you")
+	else:
+		input("I do not agree with you.")
 elif answer == "the thunder":
-	input("Okay, who is your favorite player?")
+	input("Okay, who is your favorite player on the Thunder?")
+	if answer == "kevin durant":
+		input("I agree with you.")
+	else:
+		input("I do not agree with you.")
+			  
+			  
+else:
+	input("That team is not very good")
 
