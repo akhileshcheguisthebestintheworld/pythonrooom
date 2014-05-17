a1aea9141a49b5a83eae95619ea3fed9c59de59d
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-17 09:46:59 -0700

    pythonroom update

--- avg.distancebetweenprimes.py
+++ avg.distancebetweenprimes.py
@@ -1,1 +1,17 @@
 # author: akhileshcheguisthebestintheworld
+# Find the average distance between prime numbers from 1 to 1000
+
+total=0
+LastPrime=2
+total2=0
+for n in range(2,998):
+	tests=range(2,n-1)
+	prime=True
+	for t in tests:
+		if n % t == 0:
+			prime = False
+	if prime == True:
+		total = total + (n-LastPrime)
+		LastPrime=n
+		total2 = total2 + 1		
+print total/total2	
