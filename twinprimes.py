47815bc38913a7c2f62dcb373958bf161dd867f7
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-17 13:13:50 -0700

    pythonroom update

--- twinprimes.py
+++ twinprimes.py
@@ -1,14 +1,19 @@
 # author: akhileshcheguisthebestintheworld
 total = 0
+LastPrime = 2
 total2 = 0 
 d = 0
 p1 = 2
 p2 = 3
-for n in range(2,1001):
+for n in range(2,50):
 	tests = range(2,n-1)
 	prime = True
 	for t in tests:
 		if n % t == 0:
 			prime = False
-			p1 = p2
-			p2 = 
+	if prime == True:
+		total = n - LastPrime
+		if total == 2:
+			total2 = total2 + 1
+		LastPrime = n
+print total2		
