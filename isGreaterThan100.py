0936d0872853220d71d963eda88f7507171332c9
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 19:15:41 -0700

    pythonroom update

--- isGreaterThan100.py
+++ isGreaterThan100.py
@@ -1,7 +1,8 @@
-answer = input("What do you want n to be?")
+#check if the number is greater than 100.
+answer = int(input("What do you want n to be?"))
 if answer > 100:
-	return True
+	print "Yes"
 if answer < 100:
-	return False
+	print "No"
 if answer == 100:
-	return False
+	print "equal"
