62ea222695fd0dbf9d425871df83575076a06f36
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 18:22:50 -0700

    initial commit

---
+++ isGreaterThan100.py
@@ -0,0 +1,35 @@
+# author: akhileshcheguisthebestintheworld
+def isGreaterThan100(n):
+	return False
+
+def whichIsGreater(a,b):
+	if a >b:
+		return a 
+	else:
+		return b
+def whichIsGreater(a,b,c):
+	return a
+def whichIsSmaller(a,b,c):
+	return a
+def divisibleBy(a,b):
+	return False
+def add (a,b):
+	return a
+def subtract(a,b):
+	return a
+def multiply(a,b):
+	return a
+def divide(a,b):
+	return a
+def power(a,b):
+	return a
+def absolutValue(a):
+	return a
+
+challenge
+
+def palindromeNumber(n):
+	return False
+
+def sumOFDigits(m):
+	return 0
