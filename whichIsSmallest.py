cfeae1d948daa03c402cb8edd0c11ed5f78d56c1
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 19:42:06 -0700

    pythonroom update

--- whichIsSmallest.py
+++ whichIsSmallest.py
@@ -1,16 +1,16 @@
-#determine which is greatest out of three numbers you give.
+#determine which is least out of three numbers you give.
 a = int(input("What do you want a to be?"))
 b = int(input("What do you want b to be?"))
 c = int(input("What do you want c to be?"))
-if a > b > c:
+if a < b < c:
 	print a
-if a > c > b:
+if a < c < b:
 	print a
-if b > a > c:
+if b < a < c:
 	print b
-if b > c > a:
+if b < c < a:
 	print b
-if c > a > b:
+if c < a < b:
 	print c
-if c > b > a:
+if c < b < a:
 	print c
