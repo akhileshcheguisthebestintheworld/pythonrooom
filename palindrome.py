1c1af71a94707da8755ae6abf7ebc72dc73fb6fe
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-24 11:42:10 -0700

    pythonroom update

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,13 @@
 # author: akhileshcheguisthebestintheworld
-answer = int(input("What do you want a to be?"))
-if answer(0) == answer(-1):
-	print "Yes"
+n=int(input("What do you want a to be?"))
+t=0
+r=0
+n1=n
+while n > 0:
+	t = n % 10
+	n = n / 10
+	r = r * 10 + t
+if r == n1:
+	print "palindrome"
 else:
-	print "No"
+	print "not a palindrome"
