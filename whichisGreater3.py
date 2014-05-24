4bb87d7a7a09455cccf4483c1e9116c8fe763e00
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 19:25:46 -0700

    pythonroom update

--- whichisGreater3.py
+++ whichisGreater3.py
@@ -1,6 +1,15 @@
-#add two digits
-
-answer = int(input("What do you want a to be?"))
-answer2 = int(input("What do you want b to be?"))
-answer3 = answer + answer2
-print answer3
+a = int(input("What do you want a to be?"))
+b = int(input("What do you want b to be?"))
+c = int(input("What do you want c to be?"))
+if a > b > c:
+	print a
+if a > c > b:
+	print a
+if b > a > c:
+	print b
+if b > c > a:
+	print b
+if c > a > b:
+	print c
+if c > b > a:
+	print c
