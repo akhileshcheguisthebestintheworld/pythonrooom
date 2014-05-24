035ce69d891ccb6a2163357e1abc09eb5d43b9bf
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 20:11:51 -0700

    pythonroom update

--- all.py
+++ all.py
@@ -55,3 +55,20 @@ if answer > answer2:
 	print answer
 if answer2 > answer:
 	print answer2
+	
+#determine which is least out of three numbers you give.
+a = int(input("What do you want a to be?"))
+b = int(input("What do you want b to be?"))
+c = int(input("What do you want c to be?"))
+if a < b < c:
+	print a
+if a < c < b:
+	print a
+if b < a < c:
+	print b
+if b < c < a:
+	print b
+if c < a < b:
+	print c
+if c < b < a:
+	print c	
