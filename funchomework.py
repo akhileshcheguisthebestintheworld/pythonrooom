d6532c87e16448aa85489acbd66c04538984ab4c
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 19:48:53 -0700

    pythonroom update

--- funchomework.py
+++ funchomework.py
@@ -1,20 +1,20 @@
 # author: akhileshcheguisthebestintheworld
-def isGreaterThan100(n):
-	return False
+	def isGreaterThan100(n):
+		return False
 
-def whichIsGreater(a,b):
-	if a >b:
-		return a 
-	else:
-		return b
-def whichIsGreater(a,b,c):
-	return a
-def whichIsSmaller(a,b,c):
-	return a
-def divisibleBy(a,b):
-	return False
-def add (a,b):
-	return a
+	def whichIsGreater(a,b):
+		if a >b:
+			return a 
+		else:
+			return b
+	def whichIsGreater(a,b,c):
+		return a
+	def whichIsSmaller(a,b,c):
+		return a
+	def divisibleBy(a,b):
+		return False
+	def add (a,b):
+		return a
 def subtract(a,b):
 	return a
 def multiply(a,b):
@@ -23,6 +23,7 @@ def divide(a,b):
 	return a
 def power(a,b):
 	return a
+
 def absoluteValue(a):
 	return a
 
