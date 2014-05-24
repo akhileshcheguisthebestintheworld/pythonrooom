03c1972cf541c9c70c539aafe816f1295e8fc8bf
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 20:08:19 -0700

    pythonroom update

--- all.py
+++ all.py
@@ -4,3 +4,12 @@ answer = int(input("What do you want a to be?"))
 answer2 = int(input("What do you want b to be?"))
 answer3 = answer + answer2
 print answer3
+
+# divide two numbers
+answer = int(input("What do you want a to be?"))
+answer2 = int(input("What do you want b to be?"))
+if answer > answer2:
+	answer3 = answer/answer2
+if answer2 > answer:
+	answer3 = answer2/answer
+print answer3
