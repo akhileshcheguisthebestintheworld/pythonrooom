3ec92641383adf30020c865e1942d1146fd17374
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 20:11:17 -0700

    pythonroom update

--- all.py
+++ all.py
@@ -47,3 +47,11 @@ answer = int(input("What do you want a to be?"))
 answer2 = int(input("What do you want b to be?"))
 answer3 = answer**answer2
 print answer3
+
+#determine which of two numbers that you enter is greater
+answer = int(input("What do you want a to be?"))
+answer2 = int(input("What do you want b to be?"))
+if answer > answer2:
+	print answer
+if answer2 > answer:
+	print answer2
