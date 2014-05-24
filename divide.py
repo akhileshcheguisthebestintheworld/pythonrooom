c537948b0923a108504cf02aa6984220a21bc124
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 19:54:35 -0700

    pythonroom update

--- divide.py
+++ divide.py
@@ -1,9 +1,9 @@
-# subtract two numbers
+# divide two numbers
 answer = int(input("What do you want a to be?"))
 answer2 = int(input("What do you want b to be?"))
 if answer > answer2:
-	answer3 = answer-answer2
+	answer3 = answer/answer2
 if answer2 > answer:
-	answer3 = answer2-answer
+	answer3 = answer2/answer
 print answer3	
 			 
