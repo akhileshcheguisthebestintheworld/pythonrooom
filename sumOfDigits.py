207061d17efed2161f5a61abe8c9d2aca348d2c0
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-24 11:38:09 -0700

    pythonroom update

--- sumOfDigits.py
+++ sumOfDigits.py
@@ -1,6 +1,9 @@
-# author: akhileshcheguisthebestintheworld
-answer = int(input("What do you want a to be?"))
-if answer(0) == answer(-1):
-	print "Yes"
-else:
-	print "No"
+#sum of digits in a number
+n=int(input("What do you want a to be?"))
+t=0
+s=0
+while n > 0:
+	t = n % 10
+	n = n / 10
+	s = s + t
+print s	
