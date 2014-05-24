1fb5334ff7ddba1b2edefd7c3c5406db129e73b9
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 20:08:40 -0700

    pythonroom update

--- all.py
+++ all.py
@@ -13,3 +13,14 @@ if answer > answer2:
 if answer2 > answer:
 	answer3 = answer2/answer
 print answer3
+
+# find if one number is divisible by the other or not
+a = int(input("What do you want a to be?"))
+b = int(input("What do you want b to be?"))
+if a % b == 0:
+	print "Yes"
+if b % a == 0:
+	print "Yes"
+
+else:
+	print ""
