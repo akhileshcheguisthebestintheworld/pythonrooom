47a4c78a9156800c281754e9b21cece35500638b
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-23 19:20:38 -0700

    pythonroom update

--- whichIsGreater.py
+++ whichIsGreater.py
@@ -1,35 +1,7 @@
-# author: akhileshcheguisthebestintheworld
-def isGreaterThan100(n):
-	return False
-
-def whichIsGreater(a,b):
-	if a >b:
-		return a 
-	else:
-		return b
-def whichIsGreater(a,b,c):
-	return a
-def whichIsSmaller(a,b,c):
-	return a
-def divisibleBy(a,b):
-	return False
-def add (a,b):
-	return a
-def subtract(a,b):
-	return a
-def multiply(a,b):
-	return a
-def divide(a,b):
-	return a
-def power(a,b):
-	return a
-def absoluteValue(a):
-	return a
-
-challenge
-
-def palindromeNumber(n):
-	return False
-
-def sumOFDigits(m):
-	return 0
+answer = int(input("What do you want a to be?"))
+answer2 = int(input("What do you want b to be?"))
+if answer > answer2:
+	print answer
+if answer2 > answer:
+	print answer2
+			 
