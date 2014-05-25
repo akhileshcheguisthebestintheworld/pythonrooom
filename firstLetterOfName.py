895a61dc73358146a0fefa518fb3bfdafff03bbd
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-25 13:18:40 -0700

    pythonroom update

--- firstLetterOfName.py
+++ firstLetterOfName.py
@@ -1,1 +1,11 @@
 # author: akhileshcheguisthebestintheworld
+import turtle
+t=turtle.Turtle()
+t.left(135)
+t.forward(130)
+t.left(130)
+t.forward(130)
+t.left(180)
+t.forward(65)
+t.right(60)
+t.forward(60)
