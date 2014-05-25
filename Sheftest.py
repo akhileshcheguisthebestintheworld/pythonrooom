1a95c93c56c2ec8d30e8964911d08a07e34c35b7
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-25 10:44:57 -0700

    pythonroom update

--- Sheftest.py
+++ Sheftest.py
@@ -1,6 +1,6 @@
 # testing functions
 
-print "Addition of 2 numbers"
+print "Maths Operations"
 a=int(input("What do you want your first number to be?"))
 b=int(input("What do you want your second number to be?"))
 
@@ -9,6 +9,22 @@ def add(a,b):
 	c = a+b
 	print c
 
+def subtract(a,b):
+	if a > b:
+		c = a-b
+		print c
+	if b > a:
+		c = b-a
+		print c
+	
+print "Which operation do you want?"
+print "1: Addition"
+print "2: Subtraction"
+d=int(input("Which operation do you want to do?"))
 
-add(a,b)
+if d == 1:
+	add(a,b)
+if d == 2:
+	subtract(a,b)
+	
 
