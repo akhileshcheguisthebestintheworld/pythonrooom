8d7f78742e75a0984bbc4a14acd82cf510357de1
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-25 15:04:43 -0700

    pythonroom update

--- blackhole.py
+++ blackhole.py
@@ -1,3 +1,12 @@
 import turtle
 t=turtle.Turtle()
-t.forward(50)
+colors=["red","blue","green","orange"]
+distances=range(10,100)
+t.speed(1)
+for distance in distances:
+	t.left(10)
+	for c in colors:
+		t.color(c)
+		t.forward(distance)
+		t.left(90)
+	
