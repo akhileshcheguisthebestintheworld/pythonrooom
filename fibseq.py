06d664946c4bed7a83bdc496e9af121e22c4b019
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-25 13:47:54 -0700

    pythonroom update

--- fibseq.py
+++ fibseq.py
@@ -1,11 +1,13 @@
 # author: akhileshcheguisthebestintheworld
-import turtle
-t=turtle.Turtle()
-t.left(135)
-t.forward(130)
-t.left(130)
-t.forward(130)
-t.left(180)
-t.forward(65)
-t.right(60)
-t.forward(60)
+a=0
+b=1
+c=0
+total=0
+
+while b < 1000000:
+	c=b
+	b = a + b
+	a=c
+	if a % 2 == 0:
+		total = total + a
+print total
