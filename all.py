bac8d7238a175c5e761d1ad0c654e5ba961e45bf
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-25 10:58:35 -0700

    pythonroom update

--- all.py
+++ all.py
@@ -1,22 +1,22 @@
-#add two digits
+# Selection of Maths Operations
+a=int(input("What do you want the first number to be?"))
+b = int(input("What do you want the second number to be?"))
+c = int(input("What do you want the third number to be?"))
 
-answer = int(input("What do you want a to be?"))
-answer2 = int(input("What do you want b to be?"))
-answer3 = answer + answer2
-print answer3
+#add two digits
+def add(a,b):
+	d = a + b
+	print d
 
 # divide two numbers
-answer = int(input("What do you want a to be?"))
-answer2 = int(input("What do you want b to be?"))
-if answer > answer2:
-	answer3 = answer/answer2
-if answer2 > answer:
-	answer3 = answer2/answer
-print answer3
+
+if a > b:
+	d = a/b
+if b > a:
+	d = b/a
+print d
 
 # find if one number is divisible by the other or not
-a = int(input("What do you want a to be?"))
-b = int(input("What do you want b to be?"))
 if a % b == 0:
 	print "Yes"
 if b % a == 0:
@@ -26,40 +26,30 @@ else:
 	print ""
 	
 #check if the number is greater than 100.
-answer = int(input("What do you want n to be?"))
-if answer > 100:
+if a > 100:
 	print "Yes"
-if answer < 100:
+if a < 100:
 	print "No"
-if answer == 100:
+if a == 100:
 	print "equal"
 	
 #multiply two digits
 
-answer = int(input("What do you want a to be?"))
-answer2 = int(input("What do you want b to be?"))
-answer3 = answer*answer2
-print answer3	
+d = a*b
+print c	
 
 #power 2 numbers
 
-answer = int(input("What do you want a to be?"))
-answer2 = int(input("What do you want b to be?"))
-answer3 = answer**answer2
+d = a**b
 print answer3
 
 #determine which of two numbers that you enter is greater
-answer = int(input("What do you want a to be?"))
-answer2 = int(input("What do you want b to be?"))
-if answer > answer2:
-	print answer
-if answer2 > answer:
-	print answer2
+if a > b:
+	print a
+if b > a:
+	print b
 	
 #determine which is least out of three numbers you give.
-a = int(input("What do you want a to be?"))
-b = int(input("What do you want b to be?"))
-c = int(input("What do you want c to be?"))
 if a < b < c:
 	print a
 if a < c < b:
@@ -74,9 +64,6 @@ if c < b < a:
 	print c	
 	
 #determine which is greatest out of three numbers you give.
-a = int(input("What do you want a to be?"))
-b = int(input("What do you want b to be?"))
-c = int(input("What do you want c to be?"))
 if a > b > c:
 	print a
 if a > c > b:
