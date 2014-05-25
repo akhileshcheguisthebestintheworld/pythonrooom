1b8f834b3b676d96d87b64e9e4018e227780d032
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-25 11:03:03 -0700

    pythonroom update

--- all.py
+++ all.py
@@ -9,70 +9,75 @@ def add(a,b):
 	print d
 
 # divide two numbers
-
-if a > b:
-	d = a/b
-if b > a:
-	d = b/a
-print d
+def divide(a,b):
+	if a > b:
+		d = a/b
+	if b > a:
+		d = b/a
+	print d
 
 # find if one number is divisible by the other or not
-if a % b == 0:
-	print "Yes"
-if b % a == 0:
-	print "Yes"
-
-else:
-	print ""
+def divisible(a,b):
+	if a % b == 0:
+		print "Yes"
+	if b % a == 0:
+		print "Yes"
+	
+	else:
+		print ""
 	
 #check if the number is greater than 100.
-if a > 100:
-	print "Yes"
-if a < 100:
-	print "No"
-if a == 100:
-	print "equal"
+def greaterThan100(a):
+	if a > 100:
+		print "Yes"
+	if a < 100:
+		print "No"
+	if a == 100:
+		print "equal"
 	
 #multiply two digits
-
-d = a*b
-print c	
+def multiply(a,b):
+	d = a*b
+	print c	
 
 #power 2 numbers
-
-d = a**b
-print answer3
+def power(a,b):
+	d = a**b
+	print answer3
 
 #determine which of two numbers that you enter is greater
-if a > b:
-	print a
-if b > a:
-	print b
+def greater(a,b):
+	if a > b:
+		print a
+	if b > a:
+		print b
 	
 #determine which is least out of three numbers you give.
-if a < b < c:
-	print a
-if a < c < b:
-	print a
-if b < a < c:
-	print b
-if b < c < a:
-	print b
-if c < a < b:
-	print c
-if c < b < a:
-	print c	
+def least(a,b,c):
+	if a < b < c:
+		print a
+	if a < c < b:
+		print a
+	if b < a < c:
+		print b
+	if b < c < a:
+		print b
+	if c < a < b:
+		print c
+	if c < b < a:
+		print c	
 	
 #determine which is greatest out of three numbers you give.
-if a > b > c:
-	print a
-if a > c > b:
-	print a
-if b > a > c:
-	print b
-if b > c > a:
-	print b
-if c > a > b:
-	print c
-if c > b > a:
-	print c	
+def greatest(a,b,c):
+	if a > b > c:
+		print a
+	if a > c > b:
+		print a
+	if b > a > c:
+		print b
+	if b > c > a:
+		print b
+	if c > a > b:
+		print c
+	if c > b > a:
+		print c	
