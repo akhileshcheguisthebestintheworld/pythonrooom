3d243e98dc86afc94475ce9918f5622079b326ab
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-31 16:22:59 -0700

    pythonroom update

--- robot.py
+++ robot.py
@@ -11,42 +11,14 @@ if word[0] == "what":
 		if word[2] == "pi?":
 			print"3.14159265358979323846264338299502841..."
 	if word[1] == "does":
-		if word[2] == "Mr.":
-			if word[3] == "fox":
-				if word[4] == "say?":
-					print "alingalingalingalingalingaling"
+		if word[2] == "Mr. Fox":
+			if word[3] == "say?":
+				print "alingalingalingalingalingaling"
 	if word[1] == "interests":
 		if word[2] == "you":
 			if word[3] == "robot?":
 				print "nothing"
-if word[0] == "when":
-	if word[1] == "the":
-		if word[2] == "apple":
-			if word[3] == "ate":
-				if word[4] == "the":
-					if word[5] == "banana":
-						if word[6] == "and":
-							if word[7] == "the":
-								if word[8] == "orange,":
-									if word[9] == "why":
-										if word[10] == "did":
-											if word[11] == "the":
-												if word[12] == "lion":
-													if word[13] == "eat":
-														if word[14] == "the":
-															if word[15] == "great":
-																if word[16] == "white":
-																	if word[17] == "shark":
-																		if word[18] == "who":
-																			if word[19] == "ate":
-																				if word[20] == "the":
-																					if word[21] == "fish":
-																						if word[22] == "who":
-																							if word[23] == "ate":
-																								if word[24] == "the":
-																									if word[25] == "rhino?":
-																										print "hohoho"
-																
+
 															
 										
 							
