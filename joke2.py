8e70a98fec5b633578ac379b0cdfc4a09235392f
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-31 16:31:48 -0700

    pythonroom update

--- joke2.py
+++ joke2.py
@@ -4,4 +4,4 @@ if answer == "A hippopota-mouse.":
 	input("You are correct!")
 else:
 	input("Sorry, you are wrong. Try again. What is the largest rodent in the world that has no hair on its body?")
-	if a
+	
