60dc18cd8c504a9ef641c8293b2feb4df82a5b06
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-31 14:50:19 -0700

    pythonroom update

--- homework.py
+++ homework.py
@@ -5,7 +5,7 @@ people=int(input("How many people are there?"))
 flies = int(input("How many flies are in the room?"))
 if random.randint(1,1000) < flies:
 	print "Someone is blinking."
+	pictures = pictures + 1
 else:
 	print "Nobody is blinking."
-	pictures = pictures + 1
 print pictures
