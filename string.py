d3e0c22a3126edc409d674f7400dee86cf428706
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-06-01 13:45:28 -0700

    pythonroom update

--- string.py
+++ string.py
@@ -1,11 +1,10 @@
 # author: akhileshcheguisthebestintheworld
-word = input("HI,ZOMBIE.")
+word = input("HI ZOMBIE.")
 word = word.lower()
 output = ""
 vowels = [ "a", "e", "i", "o", "u"]
 if word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[0] == "u":
-	output += word + "yay"
+	output = output+ word + "yay"
 else:
-	word[0] = word[-1]
-	del word[0]
+	output = output + word[1:] + word[0] + "ay"
 print output	
