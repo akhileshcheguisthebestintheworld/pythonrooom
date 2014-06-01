2d20a8bb5af843296448bd3e39017e56b70d1728
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-05-31 18:35:02 -0700

    pythonroom update

--- design.py
+++ design.py
@@ -1,2 +1,13 @@
-
-	
+import turtle
+t=turtle.Turtle()
+colors=["red","blue","green","orange"]
+distances=range(10,100)
+distance = int(input("What surprise shape number do you want?"))
+angle = 180 - (360/distance)
+t.speed(1000000000000000)
+for distance in distances:
+	t.left(10)
+	for c in colors:
+		t.color(c)
+		t.forward(distance)
+		t.left(angle)
