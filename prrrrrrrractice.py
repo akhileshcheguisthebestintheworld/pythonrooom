338b884ab66c97019f7cb43b69fb11b439ef43ff
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-06-07 15:56:56 -0700

    pythonroom update

--- prrrrrrrractice.py
+++ prrrrrrrractice.py
@@ -1,11 +1,9 @@
-# draw a rectangle
+# draw a triangle
 import turtle
 t = turtle.Turtle()
 t.forward(100)
-t.left(90)
-t.forward(50)
-t.left(90)
+t.left(120)
 t.forward(100)
-t.left(90)
-t.forward(50)
-t.left(90)
+t.left(120)
+t.forward(100)
+t.left(120)
