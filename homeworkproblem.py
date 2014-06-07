3bb9bb7156f4c92a50ffde2a972e3fd195eb3bfa
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-06-07 13:53:30 -0700

    pythonroom update

--- homeworkproblem.py
+++ homeworkproblem.py
@@ -1,10 +1,7 @@
-# author: akhileshcheguisthebestintheworld
-#1. Check if an inputted word is a palindrome.
 word = input("Speak, human.")
 length = len(word) + 1
 for i in range (0,length):
-	#Check if the word reads the same going forward and backward
-	#Compare the first letter with the last letter- if equal 
-		#do some code
-	#if not equal
-		# do some code
+	if word [i] == word [-i-1]: 
+		print "Palindrome."
+	else:
+		print "Not a palindrome."
