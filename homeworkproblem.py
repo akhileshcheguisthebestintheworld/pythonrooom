ded92583dd6bc5e36c38e8b3a068df3a7e84f233
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-06-07 15:30:12 -0700

    pythonroom update

--- homeworkproblem.py
+++ homeworkproblem.py
@@ -1,1 +1,14 @@
-z
+word = input("Speak, human.")
+length = len(word)
+for i in range(0,(length/2)):
+	if word[i] == word [-i-1]:
+		return True
+		palindrome = 1
+	
+	else:
+		return False
+		palindrome = 0
+if palindrome == 0:
+	print "Not a palindrome"
+else:
+	print "palindrome"
