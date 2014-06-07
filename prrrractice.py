737b37cfac5085a19fec086b77d5f71a465ac977
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-06-07 13:45:22 -0700

    pythonroom update

--- prrrractice.py
+++ prrrractice.py
@@ -1,6 +1,4 @@
-buildings = 3
-ninja1s = 25
-tunnels = 2
-ninja2s = 40
-ninjas = ninja1s * buildings + ninja2s * tunnels
-print ninjas
+a = "Akhilesh"
+b = " Chegu"
+greeting = "Hello, "
+print greeting + a + b
