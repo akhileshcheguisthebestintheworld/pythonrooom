b0ab5a472d80f6188b8664b52c0846417374fa7a
akhileshcheguisthebestintheworld <akhilesh.chegu@gmail.com>
2014-06-07 14:00:22 -0700

    pythonroom update

--- testlenfunction.py
+++ testlenfunction.py
@@ -1,7 +1,3 @@
 word = input("Speak, human.")
-length = len(word) + 1
-for i in range (0,length):
-	if word [i] == word [-i-1]: 
-		print "Palindrome."
-	else:
-		print "Not a palindrome."
+length = len(word)
+print length
